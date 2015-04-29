f71bf4346bd948ed9704b12650bed16db32a6edf
Javier Murillo <jhmmurillo@gmail.com>
2015-04-29 11:53:29 -0500

    add details.email to form and charge

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -9,16 +9,20 @@ class CheckoutsController < ApplicationController
         currency: "MXN",
         description: "Pizza Delivery at #CPMX5",
         reference_id: "001-id-CPMX5",
+        details:
+        {
+          email: params['emailBuyer']
+        },
         card: params['conektaTokenId']
       })
     rescue Conekta::ValidationError => e
-      puts e.message
+      puts e.message_to_purchaser
       #alguno de los parámetros fueron inválidos
     rescue Conekta::ProcessingError => e
-      puts e.message
+      puts e.message_to_purchaser
       #la tarjeta no pudo ser procesada
     rescue Conekta::Error
-      puts e.message
+      puts e.message_to_purchaser
       #un error ocurrió que no sucede en el flujo normal de cobros como por ejemplo un auth_key incorrecto
     end
   end
